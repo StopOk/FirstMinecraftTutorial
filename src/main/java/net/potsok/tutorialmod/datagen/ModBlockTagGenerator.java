47e1e67c8801bdafaac6780a1c97af704df91c0e
StopOK <renjinxuan@gmail.com>
2024-04-04 17:58:05 -0300

    I have made DataGens, haven't deleted json files yet but will in next commit

---
+++ src/main/java/net/potsok/tutorialmod/datagen/ModBlockTagGenerator.java
@@ -0,0 +1,48 @@
+package net.potsok.tutorialmod.datagen;
+
+import net.minecraftforge.fml.common.Mod;
+import net.potsok.tutorialmod.TutorialMod;
+import net.potsok.tutorialmod.block.ModBlocks;
+import net.potsok.tutorialmod.util.ModTags;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.common.data.BlockTagsProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModBlockTagGenerator extends BlockTagsProvider {
+    public ModBlockTagGenerator(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, @Nullable ExistingFileHelper existingFileHelper) {
+        super(output, lookupProvider, TutorialMod.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider pProvider) {
+        this.tag(ModTags.Blocks.METAL_DETECTOR_VALUABLES)
+                .add(ModBlocks.SAPPHIRE_ORE.get()).addTag(Tags.Blocks.ORES);
+
+        this.tag(BlockTags.MINEABLE_WITH_PICKAXE)
+                .add(ModBlocks.RAW_SAPPHIRE_BLOCK.get(),
+                        ModBlocks.SAPPHIRE_ORE.get(),
+                        ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get(),
+                        ModBlocks.NETHER_SAPPHIRE_ORE.get(),
+                        ModBlocks.END_STONE_SAPPHIRE_ORE.get(),
+                        ModBlocks.SAPPHIRE_BLOCK.get(),
+                        ModBlocks.RAW_SAPPHIRE_BLOCK.get(),
+                        ModBlocks.SOUND_BLOCK.get());
+
+        this.tag(BlockTags.NEEDS_IRON_TOOL)
+                .add(ModBlocks.RAW_SAPPHIRE_BLOCK.get(),
+                        ModBlocks.SAPPHIRE_ORE.get(),
+                        ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get(),
+                        ModBlocks.NETHER_SAPPHIRE_ORE.get(),
+                        ModBlocks.END_STONE_SAPPHIRE_ORE.get(),
+                        ModBlocks.SOUND_BLOCK.get());
+
+        this.tag(BlockTags.NEEDS_DIAMOND_TOOL)
+                .add(ModBlocks.SAPPHIRE_BLOCK.get());
+    }
+}
